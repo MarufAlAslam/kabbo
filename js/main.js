487f7e65762279eec3c9cf4979508fd0874468b2
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-05 23:11:34 +0600

    testimonial added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="btn btn-prev"><i class="fa fa-long-arrow-left"></i></button>',
+    nextArrow: '<button class="btn btn-next"><i class="fa fa-long-arrow-right"></i></button>'
+});
